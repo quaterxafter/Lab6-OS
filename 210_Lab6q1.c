edf60942535ad0abfa639d9dc45d5d2c34789297
quaterafter <113671539+quaterxafter@users.noreply.github.com>
2022-09-17 22:18:00 +0700

    Add files via upload

---
+++ 210_Lab6q1.c
@@ -0,0 +1,40 @@
+//64050210 วณัชญา เธียรเงิน
+#include<pthread.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+int csum;
+void *runner(void *param);
+
+int main(int argc, char *argv[]){
+
+    pthread_t tid;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_create(&tid, &attr, runner, argv[1]);
+    
+    int upperP = atoi(argv[1]);
+    int iP;
+
+    int msum = 0;
+    if(upperP > 0){
+        for(iP = 0; iP <= upperP; iP++)
+            msum += iP;
+    }
+    pthread_join(tid, NULL);
+
+    printf("csum - msum = %d\n",csum - msum);
+    return 0;
+}
+
+void *runner(void *param){
+    int upper = atoi(param);
+    int i;
+
+    csum = 0;
+    if(upper > 0){
+        for(i = 0; i<= 2*upper; i++)
+            csum += i;
+    }
+    pthread_exit(0);
+}
